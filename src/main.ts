88d7071c258e86722868d37669c38176c31cd75a
inlinefun <inlinefun@proton.me>
2026-09-20 18:39:25 +0530

    ref: clear default project

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import './assets/main.css'
-
+import '@/style.css'
+import App from '@/App.vue'
 import { createApp } from 'vue'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.mount('#app')

a88a03503c5b9177e60b0728b2e084e6b3daa191
inlinefun <inlinefun@proton.me>
2026-09-20 18:50:01 +0530

    hi :)

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,7 @@
 <script setup lang="ts"></script>
 
-<template></template>
+<template>
+  <div class="h-dvh w-full flex items-center">
+    <h1 class="w-full text-center">Hi :)</h1>
+  </div>
+</template>

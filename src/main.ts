ca243d10cbefb25b726e7841d25b94c237b5dfc1
inlinefun <inlinefun@proton.me>
2026-09-20 18:49:54 +0530

    feat: detect and use user preferred color scheme

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,14 @@ import App from '@/App.vue'
 import { createApp } from 'vue'
 
 const app = createApp(App)
-
+applyTheme()
 app.mount('#app')
+
+function applyTheme() {
+  const body = document.body
+  let theme = "light"
+  if (window.matchMedia('prefers-color-scheme: dark')) {
+    theme = "dark"
+  }
+  body.className = theme
+}
